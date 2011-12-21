8d8db79e845fcc077ed3bdabb623b62ec6828de9
Nik Krumm <nkrumm@gmail.com>
2011-12-21 10:28:57 -0800

    deleted mapperStart.sh file

--- mapperStart.sh
+++
@@ -1,6 +0,0 @@
-#qsub -l mem_requested=2G -t 1-4 -S /bin/bash -cwd -o /net/eichler/vol8/home/nkrumm/EXOMES/zeromq_test/mapper/SGE_logs -e /net/eichler/vol8/home/nkrumm/EXOMES/zeromq_test/mapper/SGE_logs -j y mapperStart.sh 
-
-source ~/.bash_profile
-module load modules modules-init modules-gs modules-eichler
-
-python mrsfast_wrapper.py e171
