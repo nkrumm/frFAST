3b0a0ff8406ff286002a0e21aa3d5516bbf97209
Nik Krumm <nkrumm@gmail.com>
2014-01-15 16:42:29 -0800

    mrsfast_wrapper: set returncode

--- mrsfast_wrapper.py
+++ mrsfast_wrapper.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
         #returnCode = subprocess.call(exec_args)
         p = subprocess.Popen(exec_args)
         pid = p.pid
-        p.wait()
+        returnCode = p.wait()
 
